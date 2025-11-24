09fcc1218c97322bc0a2156df6c0c2922800111d
Eduardo Portugal <eportugalpor@unsa.edu.pe>
2025-11-24 01:03:56 -0500

    Agregando script MultiplayerManager para administrar multijugador

---
+++ RyC/Assets/Scripts/Controllers/MultiplayerManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MultiplayerManager : MonoBehaviour
+{
+  [Header("Setup")]
+  [SerializeField] private GameObject carPrefab;
+  [SerializeField] private Transform spawnPoint1;  // Posición P1 (izquierda)
+  [SerializeField] private Transform spawnPoint2;  // Posición P2 (derecha)
+
+  private void Start()
+  {
+    // Spawn P1
+    var car1 = Instantiate(carPrefab, spawnPoint1.position, spawnPoint1.rotation);
+    var ctrl1 = car1.GetComponent<CarController>();
+    ctrl1.SetPlayerIndex(CarController.PlayerIndex.One);
+
+    // Spawn P2
+    var car2 = Instantiate(carPrefab, spawnPoint2.position, spawnPoint2.rotation);
+    var ctrl2 = car2.GetComponent<CarController>();
+    ctrl2.SetPlayerIndex(CarController.PlayerIndex.Two);
+
+    // Desactiva cámara principal si existe
+    var mainCam = Camera.main;
+    if (mainCam != null) mainCam.enabled = false;
+
+    Debug.Log("¡Multijugador iniciado! P1: WASD+Espacio | P2: Flechas+Enter");
+  }
+}
