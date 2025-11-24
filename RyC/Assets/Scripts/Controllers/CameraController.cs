74f2289ef201021e33e59fbc50d04e7018ac4f8d
Eduardo Portugal <eportugalpor@unsa.edu.pe>
2025-11-24 00:59:12 -0500

    Modificando CameraController.cs para control de diferentes camaras

--- RyC/Assets/Scripts/Controllers/CameraController.cs
+++ RyC/Assets/Scripts/Controllers/CameraController.cs
@@ -24,6 +24,32 @@ public class CameraController : MonoBehaviour
     transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
   }
 
+  private void Start()
+  {
+    cam = GetComponent<Camera>();
+    if (target != null)
+    {
+      var carCtrl = target.GetComponent<CarController>();
+      if (carCtrl != null)
+      {
+        playerIndex = carCtrl.playerIndex;
+        SetViewport();
+      }
+    }
+  }
+
+  private void SetViewport()
+  {
+    if (playerIndex == PlayerIndex.One)
+    {
+      cam.rect = new Rect(0f, 0f, 0.5f, 1f);  // Izquierda (P1)
+    }
+    else
+    {
+      cam.rect = new Rect(0.5f, 0f, 0.5f, 1f);  // Derecha (P2)
+    }
+  }
+
   public void SetTarget(Transform newTarget)
   {
     target = newTarget;
